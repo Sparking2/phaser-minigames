f0d3c33c5a69737b19d112eeb19f14f133930842
Spark <victor.cortes_marquez@proton.me>
2024-03-06 18:23:50 -0600

    :video_game: feat: Add player and coin assets, score tracking, and movement controls to main game scene in Phaser game

--- first-game/game.js
+++ first-game/game.js
@@ -1,9 +1,35 @@
 class mainScene {
-    preload(){}
+    preload() {
+        // load player
+        this.load.image('player', './assets/Slime.png');
+        this.player = this.physics.add.sprite(100, 100, 'player');
 
-    create(){}
+        // load coin
+        this.load.image('coin', './assets/Coin.png');
+        this.coin = this.physics.add.sprite(300, 300, 'coin');
+    }
 
-    update(){}
+    create() {
+        this.score = 0;
+        let style = {font: '20px Arial', fill: '#fff'};
+        this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style)
+
+        this.arrow = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if (this.arrow.right.isDown) {
+            this.player.x += 3;
+        } else if (this.arrow.left.isDown) {
+            this.player.x -= 3;
+        }
+
+        if (this.arrow.down.isDown) {
+            this.player.y += 3;
+        } else if (this.arrow.up.isDown) {
+            this.player.y -= 3;
+        }
+    }
 }
 
 new Phaser.Game({
