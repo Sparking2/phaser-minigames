b5c90b5cb31210ac43ed0d453085cf13bee84863
Spark <victor.cortes_marquez@proton.me>
2024-03-08 13:08:32 -0600

    :video_game: feat: Add player movement controls
    Player movement controls have been added to the game. The player can now move left, right, and jump up using the arrow keys. The player's velocity is adjusted based on the key press. The player can only jump if they are on the floor.

--- phaser-game/src/ts/game.ts
+++ phaser-game/src/ts/game.ts
@@ -1,17 +1,43 @@
-import { Scene } from "phaser"
+import {Scene} from "phaser"
 
 class Main extends Scene {
     private player!: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
-    preload(){
-        this.load.image('player','assets/player.png');
+    // private up: Phaser.Input.Keyboard.Key | undefined;
+    // private left: Phaser.Input.Keyboard.Key | undefined;
+    // private right: Phaser.Input.Keyboard.Key | undefined;
+    private arrow: Phaser.Types.Input.Keyboard.CursorKeys | undefined;
+
+    preload() {
+        this.load.image('player', 'assets/player.png');
     }
 
     create() {
-        this.player = this.physics.add.sprite(250,170, 'player');
+        this.player = this.physics.add.sprite(250, 170, 'player');
+        this.player.body.gravity.y = 500;
+
+        this.arrow = this.input.keyboard?.createCursorKeys();
+
+        // this.up = this.input.keyboard?.addKey("up");
+        // this.left = this.input.keyboard?.addKey("left");
+        // this.right = this.input.keyboard?.addKey("right");
     }
 
-    update(){
-        this.player.angle++;
+    update() {
+        this.movePlayer();
+    }
+
+    movePlayer() {
+        if (this.arrow?.left.isDown) {
+            this.player.body.velocity.y = -200;
+        } else if (this.arrow?.right.isDown) {
+            this.player.body.velocity.x = 200;
+        } else {
+            this.player.body.velocity.x = 0;
+        }
+
+        if (this.arrow?.up.isDown && this.player.body.onFloor()) {
+            this.player.body.velocity.y = -320
+        }
     }
 }
 
